77ae31b0c1fe9276ab6e8c944bfc8e7cad2c7bc5
andreafontaneto <andrea.fontaneto@gmail.com>
2021-11-04 15:32:12 +0100

    create object

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ Ricreiamo le card del nostro team aggiungendo al layout di base fornito, il nost
 - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte   le card del nostro team.
 - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team. (se non fate tempo a realizzare questa parte va bene ugualmente e pensateci in ogni caso solo una volta stampata correttamente tutta la pagina)
 */
+
+//creo l'oggetto singolo
+const angela = {
+
+  name: 'Angela Caroll',
+  role: 'Chief Editor',
+  image: 'angela-caroll-chief-editor.jpg'
+
+}
+
+console.log(angela);
