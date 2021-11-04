f9001588fdd663f5feeca30187726c26eb808968
andreafontaneto <andrea.fontaneto@gmail.com>
2021-11-04 15:23:13 +0100

    consegna esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('ciao');
+/*
+Ricreiamo le card del nostro team aggiungendo al layout di base fornito, il nostro javascript in cui:
+- Creiamo il nostro array di oggetti che rappresentano ciascun membro del team.
+  Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte   le card del nostro team.
+- Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team. (se non fate tempo a realizzare questa parte va bene ugualmente e pensateci in ogni caso solo una volta stampata correttamente tutta la pagina)
+*/
